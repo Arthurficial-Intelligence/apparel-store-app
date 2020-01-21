e338a0020085cea2d6795ce15df2aa5f0029606f
Arthur-Rankin <arthurrankin1992@gmail.com>
2020-01-20 18:36:12 -0600

    added checkout page, checkout-item component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,11 +2,13 @@ import { createSelector } from "reselect";
 
 const selectCart = state => state.cart;
 
+//selector to get cart items
 export const selectCartItems = createSelector(
   [selectCart],
   cart => cart.cartItems
 );
 
+//selector for quantity of products in cart for cart-icon state
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
@@ -16,7 +18,16 @@ export const selectCartItemsCount = createSelector(
     )
 );
 
+//selector for cart dropdown hidden state
 export const selectCartHidden = createSelector(
   [selectCart],
   cart => cart.hidden
+);
+
+//selector to get the total price of cart
+export const selectCartTotal = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce(
+    (acumulator, cartItem) => acumulator + cartItem.quantity * cartItem.price,
+    0
+  )
 );
