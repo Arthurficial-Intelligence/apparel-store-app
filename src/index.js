742f5a8ee700f31c7c900df296e9f25613faa9b9
Arthur-Rankin <arthurrankin1992@gmail.com>
2020-01-21 17:10:19 -0600

    added and integrated redux-persist library to keep cart data in local storage

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux"; // this is a component class that I get from react-redux that, once passed the store object, will be able to give the redux store, context to the rest of the app.
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
@@ -11,7 +12,9 @@ import App from "./App";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import roodReducer from "./root-reducer";
@@ -6,6 +7,8 @@ import roodReducer from "./root-reducer";
 //simple array that I can stick all of my middle ware into
 const middlewares = [logger];
 
-const store = createStore(roodReducer, applyMiddleware(...middlewares));
+export const store = createStore(roodReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
