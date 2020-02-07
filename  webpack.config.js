0c03d6d7a17bc4706f8f67d581e81e0ae69c5f29
Arthur-Rankin <arthurrankin1992@gmail.com>
2020-02-06 18:50:39 -0600

    document experimentation for styleguideist

---
+++  webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    template: {
+      head: {
+        links: [
+          {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&display=swap'
+          }
+        ]
+      }
+    },
+    theme: {
+      fontFamily: {
+        base: '"Open Sans Condensed", sans-serif'
+      }
+    }
+  }
---
+++ src/components/custom-button/Readme.md
@@ -0,0 +1,12 @@
+##### CustomButton Basic example:
+```jsx
+<CustomButton>Push Me</CustomButton>
+```
+
+##### Prop: inverted, isGoogleSignIn
+```jsx
+<CustomButton inverted >INVERTED</CustomButton> 
+<br/>
+<CustomButton isGoogleSignIn >SIGN IN WITH GOOGLE</CustomButton>
+```
+
